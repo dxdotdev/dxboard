624df280d5cd4575b3b43c1220066dd8848fbee0
Davi Reis <dreisss.dev@gmail.com>
2024-11-13 19:54:32 -0300

    feat: add swapy and basic structure and working

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config'
 import alpinejs from '@astrojs/alpinejs'
 import tailwind from '@astrojs/tailwind'
 
+import solidJs from '@astrojs/solid-js'
+
 export default defineConfig({
   integrations: [
     tailwind(),
     alpinejs({
       entrypoint: 'alpine.config.ts',
     }),
+    solidJs(),
   ],
 })
